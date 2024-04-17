5386d6fd0bb3bd8401242396a766ccd051c668e2
HUYTER <hackde15@outlook.com>
2024-04-16 23:49:49 -0300

    ATUALIZANDO ARQUIVOS

--- main.js
+++ main.js
@@ -1,20 +1,13 @@
-
-document.getElementById("formContato").addEventListener("submit", function(event) {
-    event.preventDefault(); // Impede o envio do formulário padrão
-
-    // Obtém os valores do formulário
-    var email = document.getElementById("email").value;
-    var telefone = document.getElementById("telefone").value;
-
-    // Cria uma nova linha na tabela
-    var tabela = document.getElementById("corpoTabela");
-    var novaLinha = tabela.insertRow();
-    var celulaEmail = novaLinha.insertCell(0);
-    var celulaTelefone = novaLinha.insertCell(1);
-    celulaEmail.textContent = email;
-    celulaTelefone.textContent = telefone;
-
-    // Limpa os campos do formulário
-    document.getElementById("email").value = "";
-    document.getElementById("telefone").value = "";
-});
+$(document).ready(function() {
+    $('#task-form').submit(function(event) {
+      event.preventDefault();
+      var taskName = $('#task-input').val();
+      $('#task-list').append('<li>' + taskName + '</li>');
+      $('#task-input').val('');
+    });
+  
+    $('#task-list').on('click', 'li', function() {
+      $(this).toggleClass('completed');
+    });
+  });
+  
